db421536833fee9542b31f48ff2145bdd1ec7057
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2016-12-23 16:10:58 +0200

    f

---
+++ js/main.js
@@ -0,0 +1,151 @@
+// Global parameters
+window.params = {
+    isMobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+    isIOS: /iPhone|iPad|iPod/i.test(navigator.userAgent)
+};
+
+
+jQuery(document).ready(function($) {
+
+    /*---------------------------
+                                  ADD CLASS ON SCROLL
+    ---------------------------*/
+    $(function() { 
+        var $document = $(document),
+            $element = $('.menu-button'),
+            $element2 = $('header'),
+            className = 'hasScrolled';
+
+        $document.scroll(function() {
+            $element.toggleClass(className, $document.scrollTop() >= 1);
+            $element2.toggleClass(className, $document.scrollTop() >= 1);
+        });
+    });
+    
+    /*---------------------------
+                                PAGE ANCHORS
+    ---------------------------*/
+    $('.mainNav a, .anchor').click(function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 50
+        }, 800);
+        return false;
+    });
+
+    /*---------------------------
+                                ACTIVATE MENU ITEM OVER CURRENT SECTION
+    ---------------------------*/
+    var $sections = $('section');
+    $(window).scroll(function(){
+        var currentScroll = $(this).scrollTop();
+        var $currentSection;
+        var windowHalf = $(window).height() / 2;
+        
+        $sections.each(function(){
+          var divPosition = $(this).offset().top - windowHalf;
+          
+          if( divPosition - 1 < currentScroll ){
+            $currentSection = $(this);
+          }
+        var id = $currentSection.attr('id');
+          $('a').removeClass('active');
+          $("[href=#"+id+"]").addClass('active');
+        })
+    });
+
+    /*---------------------------
+                                  MENU TOGGLE
+    ---------------------------*/
+    $('.menu-button').on('click', function(event) {
+        event.preventDefault();
+        $(this).toggleClass('active');
+        $(this).siblings('header').toggleClass('active');
+        if ($('header').hasClass('active')) {
+                $('body').css('overflow', 'hidden');
+            } else {
+                $('body').css('overflow', 'visible');
+            }
+    });
+
+
+
+    /*---------------------------
+                                  Magnific popup
+    ---------------------------*/
+    $('.magnific').magnificPopup({
+        type: 'inline',
+
+        fixedContentPos: false,
+        fixedBgPos: true,
+
+        overflowY: 'auto',
+        modal: false,
+
+        closeBtnInside: true,
+        preloader: false,
+        
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
+    });
+
+
+
+    /*----------------------------
+                              SEND FORM
+    -------------------------*/
+    /**
+     *
+     * Open popup
+     *
+     * @param popup {String} jQuery object (#popup)
+     *
+     * @return n/a
+     *
+    */
+    function openPopup(popup){
+        $.magnificPopup.open({
+            items: {
+              src: popup
+            },
+            type: 'inline',
+            fixedContentPos: false,
+            fixedBgPos: true,
+            overflowY: 'auto',
+            modal: false,
+            closeBtnInside: true,
+            preloader: false,
+            midClick: true,
+            removalDelay: 300,
+            mainClass: 'my-mfp-slide-bottom'
+        }, 0);
+    }
+
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+        var data = $(this).serialize();
+        $.ajax({
+            url: theme.url + '/forms.php',
+            type: 'POST',
+            data: data,
+            success: function(result){
+                if (result.status == 'ok') {
+                    openPopup('#modal-popup-ok')
+                } else {
+                    openPopup('#modal-popup-error')
+                }
+            },
+            error: function(result) {
+                openPopup('#modal-popup-error');
+            }
+        })
+        .always(function() {
+            $('form').each(function(index, el) {
+                $(this)[0].reset();
+            });
+        });
+        
+    });
+
+}); // end file
